41a833659021bb4f4a4b2ecd148628fda9814e4d
Stephen Kenworthy <steveyken@gmail.com>
2023-10-14 13:19:14 +0800

    Fixed tabs

--- lib/ffcrm_endpoint/engine.rb
+++ lib/ffcrm_endpoint/engine.rb
@@ -1,17 +1,13 @@
-require 'fat_free_crm'
-
 module FfcrmEndpoint
   class Engine < ::Rails::Engine
 
     config.to_prepare do
+
       require "ffcrm_endpoint/endpoint"
 
-      ActiveSupport.on_load(:fat_free_crm_view_factory) do
-        tab_urls = ::FatFreeCRM::Tabs.admin.map{|tab| tab[:url]}.map{|url| url[:controller]}
-        unless tab_urls.include? 'admin/ffcrm_endpoint'
-          tab_urls = tab_urls.dup << { url: { controller: "admin/ffcrm_endpoint" }, text: "Endpoints", icon: 'fa-long-arrow-down'}
-          ::FatFreeCRM::Tabs.admin = tab_urls
-        end
+      tab_urls = FatFreeCRM::Tabs.admin.map{|tab| tab[:url]}.map{|url| url[:controller]}
+      unless tab_urls.include? 'admin/ffcrm_endpoint'
+        FatFreeCRM::Tabs.admin << {url: { controller: "admin/ffcrm_endpoint" }, text: "Endpoints", icon: 'fa-long-arrow-down'}
       end
 
     end
