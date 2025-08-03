ad528a5b45f6127c7e82dc476127e301b556a1f9
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-03 03:38:45 +0000

    Rubocop of spec

--- spec/controllers/endpoints_controller_spec.rb
+++ spec/controllers/endpoints_controller_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe FfcrmEndpoint::EndpointsController, type: :controller do
-
   let(:endpoint) { double(FfcrmEndpoint::Endpoint) }
 
-  describe "consume" do
-
-    it "should call process when authenticate returns true" do
+  describe 'consume' do
+    it 'should call process when authenticate returns true' do
       expect(endpoint).to receive(:process)
       expect(endpoint).to receive(:authenticate).and_return(true)
       allow(controller).to receive(:endpoint).and_return(endpoint)
@@ -14,14 +12,12 @@ RSpec.describe FfcrmEndpoint::EndpointsController, type: :controller do
       expect(response.status).to eql(201)
     end
 
-    it "should not call process when authenticate returns false" do
+    it 'should not call process when authenticate returns false' do
       expect(endpoint).not_to receive(:process)
       expect(endpoint).to receive(:authenticate).and_return(false)
       expect(controller).to receive(:endpoint).and_return(endpoint)
       get :consume, params: { klass_name: :my_custom_endpoint, format: :js }
       expect(response.status).to eql(401)
     end
-
   end
-
 end
--- spec/lib/endpoint_spec.rb
+++ spec/lib/endpoint_spec.rb
@@ -1,34 +1,26 @@
 require 'rails_helper'
 
 describe FfcrmEndpoint::Endpoint do
-
-  describe "initialize" do
-
-    it "should initialize params"
-    it "should raise an error if action is blank" do
+  describe 'initialize' do
+    it 'should initialize params'
+    it 'should raise an error if action is blank' do
       # expect { something }.to raise_error(SomeError)
     end
-
   end
 
-  describe "process" do
+  describe 'process' do
+    it 'should find the subclass'
 
-    it "should find the subclass"
+    it 'should call process on the subclass'
 
-    it "should call process on the subclass"
-
-    it "should raise an error if it cannot find the subclass"
-
+    it 'should raise an error if it cannot find the subclass'
   end
 
-  describe "config" do
-
-    it "should provide access to subclass settings"
-
+  describe 'config' do
+    it 'should provide access to subclass settings'
   end
 
-  it "should register a subclass"
-  it "should return the subclass class"
-  it "should return the subclass class name"
-
+  it 'should register a subclass'
+  it 'should return the subclass class'
+  it 'should return the subclass class name'
 end
