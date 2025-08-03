748ead37d1ab2e1b8980947e22840faeeb10da6a
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-03 02:37:05 +0000

    Stub out in spec

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  # Stub out authentication, so when running as an engine we get no complaints about skipping a callback that
+  # doesn't exist
+  def authenticate_user!
+    true
+  end
 end
