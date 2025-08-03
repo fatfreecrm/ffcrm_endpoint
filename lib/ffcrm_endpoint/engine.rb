dea25cf7892d8f83207df0745de9a3d11333b123
Daniel O'Connor <daniel.oconnor@gmail.com>
2025-08-03 13:04:20 +0930

    Improve readability

--- lib/ffcrm_endpoint/engine.rb
+++ lib/ffcrm_endpoint/engine.rb
@@ -2,22 +2,23 @@ module FfcrmEndpoint
   class Engine < ::Rails::Engine
 
     config.to_prepare do
-
       require "ffcrm_endpoint/endpoint"
 
-      tab_urls = FatFreeCRM::Tabs.admin.map{|tab| tab[:url]}.map{|url| url[:controller]}
+      tab_urls = FatFreeCRM::Tabs.admin.map { |tab| tab[:url] }.map{ |url| url[:controller] }
       unless tab_urls.include? 'admin/ffcrm_endpoint'
-        FatFreeCRM::Tabs.admin << {url: { controller: "admin/ffcrm_endpoint" }, text: "Endpoints", icon: 'fa-long-arrow-down'}
+        FatFreeCRM::Tabs.admin << {
+          url: { controller: "admin/ffcrm_endpoint" },
+          text: "Endpoints",
+          icon: 'fa-long-arrow-down'
+        }
       end
-
     end
 
     config.generators do |g|
-      g.test_framework      :rspec,        :fixture => false
+      g.test_framework :rspec, :fixture => false
       g.fixture_replacement :factory_bot, :dir => 'spec/factories'
       g.assets false
       g.helper false
     end
-
   end
 end
